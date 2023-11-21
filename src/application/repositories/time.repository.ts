ebd8ab41fdc0c39a7e1ee562b1bcb695a4c80a58
Rafael Pereira <rafaelsantospereira03@gmail.com>
2023-11-21 11:33:03 -0300

    feature: Correção para deploy na AWS

--- src/application/repositories/time.repository.ts
+++ src/application/repositories/time.repository.ts
@@ -1,10 +1,9 @@
 import { CreateTimeDto } from '@/infra/http/modules/time/dtos/create-or-update-time.dto';
-import { Time } from '../entities/Time';
 
 export interface ITimeRepository {
-  create(Time: Time): Promise<Time>;
+  create(Time: any): Promise<any>;
   remove(timeId: string): Promise<void>;
-  findById(time_id: string): Promise<Time>;
-  findAll(organization_id: string): Promise<Time[]>;
-  update(timeId: string, timeDto: CreateTimeDto): Promise<Time>;
+  findById(time_id: string): Promise<any>;
+  findAll(organization_id: string): Promise<any[]>;
+  update(timeId: string, timeDto: CreateTimeDto): Promise<any>;
 }
